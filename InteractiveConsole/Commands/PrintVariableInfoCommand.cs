bc103d7e33c5e1658411d780053206337a3a94db
sharklasers996 <sharklasers996@gmail.com>
2019-12-22 08:21:08 +0200

    Added printing var contents

--- InteractiveConsole/Commands/PrintVariableInfoCommand.cs
+++ InteractiveConsole/Commands/PrintVariableInfoCommand.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using InteractiveConsole.Attributes;
 using InteractiveConsole.Extensions;
@@ -9,6 +10,7 @@ namespace InteractiveConsole.Commands
     public class PrintVariableInfoCommand : BaseCommand
     {
         [CommandParameter]
+        [Required]
         public InMemoryStorageVariable Variable { get; set; }
 
         public override object Execute()
@@ -30,6 +32,24 @@ namespace InteractiveConsole.Commands
             }
 
             Printer.WriteLine().Info($"Variable {Variable} is a {variable.ToTypeString()} returned by {variable.ProducedByCommand}");
+
+            if (Variable.TypeInfo.IsList)
+            {
+                Printer.WriteLine().Info("Variable contents: ");
+                Printer.NewLine();
+                for (var i = 0; i < Variable.Length; i++)
+                {
+                    var listItem = Variable.TypeInfo.Type.GetProperty("Item").GetValue(Variable.Value, new object[] { i });
+                    Printer.WriteLine().Info2($"#{i}: {listItem.ToString()}");
+                    Printer.NewLine();
+                }
+            }
+            else if (!String.IsNullOrEmpty(Variable.ValueString.Trim()))
+            {
+                Printer.WriteLine().Info("Variable content: ");
+                Printer.NewLine();
+                Printer.WriteLine().Info2(Variable.ValueString);
+            }
 
             return null;
         }
