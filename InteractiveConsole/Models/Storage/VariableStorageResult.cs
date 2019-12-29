d5376e6556acbeeb831f3bf4fbcf1f8680d03a55
sharklasers996 <sharklasers996@gmail.com>
2019-12-29 08:35:07 +0200

    Removed unused class

--- InteractiveConsole/Models/Storage/VariableStorageResult.cs
+++
@@ -1,8 +0,0 @@
-namespace InteractiveConsole.Models.Storage
-{
-    public class VariableStorageResult
-    {
-        public InMemoryStorageVariable Variable { get; set; }
-        public bool Persisted { get; set; }
-    }
-}
